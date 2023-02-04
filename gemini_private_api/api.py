a0c600ef647aad7e4396495901180bda56e91fab
Michal Polit <mpolit@protonmail.com>
2023-02-04 11:54:31 +0000

    Add method and test stubs. | Fixes #1

---
+++ gemini_private_api/api.py
@@ -0,0 +1,159 @@
+class GeminiPrivateAPI:
+    def __int__(self):
+        pass
+
+    def new_order(self):
+        pass
+
+    def cancel_order(self):
+        pass
+
+    def wrap_order(self):
+        pass
+
+    def cancel_all_session_orders(self):
+        pass
+
+    def cancel_all_active_orders(self):
+        pass
+
+    def order_status(self):
+        pass
+
+    def get_active_orders(self):
+        pass
+
+    def get_past_trades(self):
+        pass
+
+    def get_notional_volume(self):
+        pass
+
+    def get_trade_volume(self):
+        pass
+
+    def fx_rate(self):
+        pass
+
+    def new_clearing_order(self):
+        pass
+
+    def new_broker_order(self):
+        pass
+
+    def clearing_order_status(self):
+        pass
+
+    def cancel_clearing_order(self):
+        pass
+
+    def confirm_clearing_order(self):
+        pass
+
+    def clearing_order_list(self):
+        pass
+
+    def clearing_broker_list(self):
+        pass
+
+    def clearing_trades(self):
+        pass
+
+    def get_available_balances(self):
+        pass
+
+    def get_notional_balances(self):
+        pass
+
+    def transfers(self):
+        pass
+
+    def transactions(self):
+        pass
+
+    def custody_account_fees(self):
+        pass
+
+    def get_deposit_address(self):
+        pass
+
+    def new_deposit_address(self):
+        pass
+
+    def withdraw_crypto_funds(self):
+        pass
+
+    def gas_fee_estimation(self):
+        pass
+
+    def internal_transfers(self):
+        pass
+
+    def add_bank(self):
+        pass
+
+    def add_bank_cad(self):
+        pass
+
+    def payment_methods(self):
+        pass
+
+    def sen_withdrawals(self):
+        pass
+
+    def get_earn_balances(self):
+        pass
+
+    def get_earn_rates(self):
+        pass
+
+    def get_earn_interest(self):
+        pass
+
+    def get_earn_history(self):
+        pass
+
+    def get_staking_balances(self):
+        pass
+
+    def get_staking_rates(self):
+        pass
+
+    def get_staking_rewards(self):
+        pass
+
+    def get_staking_history(self):
+        pass
+
+    def staking_deposits(self):
+        pass
+
+    def staking_withdrawals(self):
+        pass
+
+    def create_address_request(self):
+        pass
+
+    def view_approved_addresses(self):
+        pass
+
+    def remove_addresses_from_approved_address_list(self):
+        pass
+
+    def account_detail(self):
+        pass
+
+    def create_account(self):
+        pass
+
+    def rename_account(self):
+        pass
+
+    def get_accounts_in_master_group(self):
+        pass
+
+    def using_master_api_keys(self):
+        pass
+
+    def heartbeat(self):
+        pass
--- tests/test_gemini_private_api.py
+++ tests/test_gemini_private_api.py
@@ -1,5 +1,206 @@
-from gemini_private_api import __version__
+def test_new_order():
+    assert False
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_cancel_order():
+    assert False
+
+
+def test_wrap_order():
+    assert False
+
+
+def test_cancel_all_session_orders():
+    assert False
+
+
+def test_cancel_all_active_orders():
+    assert False
+
+
+def test_order_status():
+    assert False
+
+
+def test_get_active_orders():
+    assert False
+
+
+def test_get_past_trades():
+    assert False
+
+
+def test_get_notional_volume():
+    assert False
+
+
+def test_get_trade_volume():
+    assert False
+
+
+def test_fx_rate():
+    assert False
+
+
+def test_new_clearing_order():
+    assert False
+
+
+def test_new_broker_order():
+    assert False
+
+
+def test_clearing_order_status():
+    assert False
+
+
+def test_cancel_clearing_order():
+    assert False
+
+
+def test_confirm_clearing_order():
+    assert False
+
+
+def test_clearing_order_list():
+    assert False
+
+
+def test_clearing_broker_list():
+    assert False
+
+
+def test_clearing_trades():
+    assert False
+
+
+def test_get_available_balances():
+    assert False
+
+
+def test_get_notional_balances():
+    assert False
+
+
+def test_transfers():
+    assert False
+
+
+def test_transactions():
+    assert False
+
+
+def test_custody_account_fees():
+    assert False
+
+
+def test_get_deposit_address():
+    assert False
+
+
+def test_new_deposit_address():
+    assert False
+
+
+def test_withdraw_crypto_funds():
+    assert False
+
+
+def test_gas_fee_estimation():
+    assert False
+
+
+def test_internal_transfers():
+    assert False
+
+
+def test_add_bank():
+    assert False
+
+
+def test_add_bank_cad():
+    assert False
+
+
+def test_payment_methods():
+    assert False
+
+
+def test_sen_withdrawals():
+    assert False
+
+
+def test_get_earn_balances():
+    assert False
+
+
+def test_get_earn_rates():
+    assert False
+
+
+def test_get_earn_interest():
+    assert False
+
+
+def test_get_earn_history():
+    assert False
+
+
+def test_get_staking_balances():
+    assert False
+
+
+def test_get_staking_rates():
+    assert False
+
+
+def test_get_staking_rewards():
+    assert False
+
+
+def test_get_staking_history():
+    assert False
+
+
+def test_taking_deposits():
+    assert False
+
+
+def test_staking_withdrawals():
+    assert False
+
+
+def test_create_address_request():
+    assert False
+
+
+def test_view_approved_addresses():
+    assert False
+
+
+def test_remove_addresses_from_approved_address_list():
+    assert False
+
+
+def test_account_detail():
+    assert False
+
+
+def test_create_account():
+    assert False
+
+
+def test_rename_account():
+    assert False
+
+
+def test_get_accounts_in_master_group():
+    assert False
+
+
+def test_using_master_api_keys():
+    assert False
+
+
+def test_heartbeat():
+    assert False
